6b7a2d5772f265d066cc826957ea7906059cc0f8
ShauryaDhawan0786 <95754165+ShauryaDhawan0786@users.noreply.github.com>
2024-03-18 11:28:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+const response = {
+	is_success: true,
+	user_id: 'shaurya_dhawan_23042003',
+	email: 'shaurya1305.be21@chitkara.edu.in',
+	roll_number: 2110991305,
+	odd_numbers: [],
+	even_numbers: [],
+	alphabets: []
+}
+
+app.post('/bfhl', (req, res) => {
+	for (let i = 0; i < req.body.length; i++) {
+		if (typeof req.body[i] === 'number') {
+			if (req.body[i] % 2 == 0) {
+				response.even_numbers.add(req.body[i])
+			} else {
+				response.odd_numbers.add(req.body[i])
+			}
+		} else if (typeof req.body[i] === 'string') {
+			response.alphabets.add(req.body[i])
+		}
+	}
+
+	return res.status(200).json(response)
+})
+
+app.listen(5000, () => {
+	console.log('Server Started')
+})
